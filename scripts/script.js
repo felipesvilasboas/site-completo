480fa7ef3a40ffb55860810d2f2a7e68a91604da
Felipe Vilas Boas <102095497+felipesvilasboas@users.noreply.github.com>
2022-04-13 18:19:22 -0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,51 @@ function entrar() {
 function sair() {
     escrever.innerHTML = 'Quem é Felipe Vilas Boas?'
 }
+
+let fundocaixa = document.querySelector('#fundocaixa')
+let estilo02 = document.querySelector('#estilo02')
+let estilo03 = document.querySelector('#estilo03')
+estilo02.addEventListener('click', clicar);
+estilo03.addEventListener('click', clicar02);
+estilo02.addEventListener('dblclick', dblclicar);
+estilo03.addEventListener('dblclick', dblclicar02);
+
+function clicar() {
+    fundocaixa.style.backgroundImage = 'linear-gradient(#CB6CE6, #CB6CE6)'
+    escrever.style.color = '#6349EB'
+}
+
+function clicar02() {
+    fundocaixa.style.backgroundImage = 'linear-gradient(#7ED957, #7ED957)'
+    escrever.style.color = '#358175'
+}
+
+function dblclicar() {
+    location.reload()
+}
+
+function dblclicar02() {
+    location.reload()
+}
+
+function pegadinha() {
+    let data = new Date()
+    var hora = data.getHours()
+    if (hora >= 0 && hora < 12) {
+        hora = 'bom dia'
+    } else if (hora >= 12 && hora <18) {
+        hora = 'boa tarde'
+    } else {
+        hora = 'boa noite'
+    }
+    let qualnome = prompt (`Olá, ${hora}. Qual seu nome?`)
+    if (qualnome.length == 0) {
+        alert('Opa, esqueceu de digitar seu nome :)')
+        let qualnome = prompt (`E então, qual seu nome?`)
+        alert (`Tudo bem ${qualnome}, vou te passar meu contato.`)
+        window.open ('https://google.com')
+    } else {
+        alert (`Tudo bem ${qualnome}, vou te passar meu contato.`)
+        window.open ('https://google.com')
+    }
+}
